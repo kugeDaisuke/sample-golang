a2e5cfcffe112d5c9172e399f02bde7aa5eefb79
KugeDaisuke <s8103@enecom-th.jp>
2023-11-17 11:36:18 +0900

    課題3完了

--- sample_api/router/router.go
+++ sample_api/router/router.go
@@ -23,22 +23,29 @@ func Route(e *echo.Echo) {
 	e.GET("", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Accessible")
 	})
+	// e.Logger.Fatal(e.Start(":1324"))
 
 	// クライアントからhttp://{バックエンドのURL}:{バックエンドのポート}/helloが
 	// リクエストされたときに、handler/hello.goのHello関数を実行する
 	e.GET("/hello", handler.Hello)
+	// e.Logger.Fatal(e.Start(":1324"))
 
 	// サーバーを起動して、以下のコマンドをターミナルから実行しましょう
 	// curl -X PUT -H "Content-Type: application/json" -d "{\"Name\": \"smart phone\"}" localhost:1324/items
 	e.PUT("/items", handler.CreateItem)
+	// e.Logger.Fatal(e.Start(":1324"))
 
 	// 課題１ handlerディレクトリにget_item.goを作成し、ブラウザに文字列"smart phone"を表示させる
 	e.GET("/item", handler.GetItem)
+	// e.Logger.Fatal(e.Start(":1324"))
 
 	// 課題２ entityに新しく構造体を作成して、任意のテーブルを作成し、データを登録する
 	// curl -X PUT -H "Content-Type: application/json" -d "{\"Name\": \"一郎\", \"Email\": \"s1111@enecom-th.jp\", \"Pass\": \"pass1\"}" localhost:1324/users
 	e.PUT("/users", handler.CreateUser)
+	// e.Logger.Fatal(e.Start(":1324"))
 
 	// 課題３ 課題２で作成したテーブルにデータを入力し、そのデータを更新する処理を作成する
-	e.POST("/user_update", handler.UpdateUser)
+	// curl -X POST -H "Content-Type: application/json" -d "{\"Name\": \"三郎\", \"Email\": \"s3333@enecom-th.jp\", \"Pass\": \"pass3\"}" localhost:1324/userup
+	e.POST("/userup", handler.UpdateUser)
+	// e.Logger.Fatal(e.Start(":1324"))
 }
